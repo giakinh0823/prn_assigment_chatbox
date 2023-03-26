6c0640a0cd1951378883e68376216a3ab005d124
Hà Gia Kính <giakinh2000@gmail.com>
2023-03-26 18:38:26 +0700

    update

--- prn_job_manager/Migrations/20230326113721_UpdateDatabase-637.Designer.cs
+++ prn_job_manager/Migrations/20230326113721_UpdateDatabase-637.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using prn_job_manager.Models;
 
@@ -11,9 +12,10 @@ using prn_job_manager.Models;
 namespace prn_job_manager.Migrations
 {
     [DbContext(typeof(cron_jobContext))]
-    partial class cron_jobContextModelSnapshot : ModelSnapshot
+    [Migration("20230326113721_UpdateDatabase-637")]
+    partial class UpdateDatabase637
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
