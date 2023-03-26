b3f41aaa44ef451ef69760381523b245bf8af139
unknown <donghiem114@gmail.com>
2023-03-26 18:47:56 +0700

    fix

---
+++ prn_job_manager/Pages/UserPage/MonitorsSearch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace prn_job_manager.Pages.UserPage
+{
+    public class MonitorsSearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
